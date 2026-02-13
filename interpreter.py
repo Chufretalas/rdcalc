f7018e5ace6ae737df7726167cfb1b58b35c7d73
Chufretalas <101844490+Chufretalas@users.noreply.github.com>
2026-02-13 08:58:29 -0300

    finished the interpreter

---
+++ interpreter.py
@@ -0,0 +1,58 @@
+import math
+from typing import cast
+
+from expr import BinaryExpr, Expr, ExprVisitor, GroupingExpr, LiteralExpr, UnaryExpr
+from token_type import TokenType
+
+
+class Interpreter(ExprVisitor[float]):
+    def do_the_math_stuff(self, expr: Expr) -> float:
+        return cast(float, expr.accept(self))
+
+    def visitBinaryExpr(self, expr: BinaryExpr) -> float:
+        if (
+            expr.operator.type == TokenType.POWER
+        ):  # Respecting expected solve order (even though it doesn't matter since this calculator does not produce side-effects)
+            right_value = cast(float, expr.right.accept(self))
+            left_value = cast(float, expr.left.accept(self))
+        else:
+            left_value = cast(float, expr.left.accept(self))
+            right_value = cast(float, expr.right.accept(self))
+
+        match expr.operator.type:
+            case TokenType.PLUS:
+                return left_value + right_value
+            case TokenType.MINUS:
+                return left_value - right_value
+            case TokenType.STAR:
+                return left_value * right_value
+            case TokenType.SLASH:
+                if right_value == 0.0:
+                    raise Exception(
+                        f"Can not divide by zero. {left_value} / {right_value}"
+                    )
+                return left_value / right_value
+            case TokenType.POWER:
+                return left_value**right_value
+
+        raise Exception("Unknown case in visitBinaryExpression")
+
+    def visitUnaryExpr(self, expr: UnaryExpr) -> float:
+        value = cast(float, expr.expr.accept(self))
+        match expr.operator.type:
+            case TokenType.MINUS:
+                return -value
+            case TokenType.BANG:
+                if not float.is_integer(value):
+                    raise Exception(
+                        f"Factorial can only be applied to an integer. Expected integer, got {value}"
+                    )
+                return float(math.factorial(int(value)))
+
+        raise Exception("Unknown case in visitUnaryExpression")
+
+    def visitGroupingExpr(self, expr: GroupingExpr) -> float:
+        return cast(float, expr.expr.accept(self))
+
+    def visitLiteralExpr(self, expr: LiteralExpr) -> float:
+        return expr.number
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from ast_printer import AstPrinter
+from interpreter import Interpreter
 from lexer import Lexer
 from parser import Parser
 
 try:
     ast_printer = AstPrinter()
+    interpreter = Interpreter()
     while True:
         input_str = input("> ")
         lexer = Lexer(input_str)
@@ -11,6 +13,7 @@ try:
         parser = Parser(tokens)
         expr = parser.parse()
         ast_printer.print(expr)
-        
+        print(interpreter.do_the_math_stuff(expr))
+
 except KeyboardInterrupt:
-    print("Bye!")
+    print("\nBye!")
